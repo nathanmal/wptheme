f5a16e024eb9c8d42c4569d597c2ab5fd93dd326
Nathan Malachowski <nathanmal@gmail.com>
2018-01-18 15:05:35 -0600

    moved helper functions to Theme

--- library/helpers.php
+++ library/helpers.php
@@ -1,18 +1,2 @@
 <?php 
-
-
-if( ! function_exists('wptheme_template_exists') )
-{
-	function wptheme_template_exists($name)
-	{
-		return file_exists( 'views/templates/' . $name . '.php' );
-	}
-}
-
-if( ! function_exists('wptheme_load_template') )
-{
-	function wptheme_load_template($name)
-	{
-		include( 'views/templates/' . $name . '.php' );
-	}
-}
+// Helper functions
