a71efff6d55177890ceee9331aee7da54e09031c
Nathan Malachowski <nathanmal@gmail.com>
2018-03-20 17:17:47 -0500

    update to index template, sass comments

--- views/templates/index.php
+++ views/templates/index.php
@@ -1,33 +1,29 @@
 <?php Theme::partial('navbar'); ?>
 
 <main id="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Webpage">
+	<div class="container-fluid no-padding" tabindex="-1">
+		<div class="row">
+			<!-- Begin Loop -->
+			<?php 
 
-<div class="container-fluid no-padding" tabindex="-1">
+			if ( have_posts() ) 
+			{
+				while ( have_posts() ) : the_post();
+					// Check post format
+					$format = get_post_format();
+					// If not used, default with post
+					if( empty($format) ) $format = 'post';
+					// Include format view
+					Theme::view('formats/'.$format);
+				endwhile;
 
-	<div class="row">
-	
-		<?php 
+			} else {
+				// Show missing content message if there are no posts
+				Theme::partial('missing'); 
+			}
 
-		if ( have_posts() ) 
-		{
-			// BEGIN LOOP
-			while ( have_posts() ) : the_post();
-				$format = get_post_format();
-
-				if( empty($format) ) $format = 'post';
-
-				Theme::view('formats/'.$format);
-			endwhile;
-
-		} else {
-			// Show missing content message if there are no posts
-			Theme::partial('missing'); 
-		}
-
-		?>	
-
-	</div> <!-- End .row -->
-
-</div> <!-- End #wrapper -->
-
+			?>	
+			<!-- End Loop -->
+		</div>
+	</div>
 </main>
