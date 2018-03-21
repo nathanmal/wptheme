da7b820166b1efe073b831886e584a3dda4816bb
Nathan Malachowski <nathanmal@gmail.com>
2018-03-20 21:18:02 -0500

    updates to banner

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,15 +1,17 @@
 <?php
 // Get page/post ID
 $id  = get_the_ID();
+// Pass this to override
+$banner = isset($banner) ? $banner : array();
 // Try to get featured image first
-$img = get_the_post_thumbnail_url($id,'full');
+$img = isset($banner['image']) ? $banner['image'] : get_the_post_thumbnail_url($id,'full');
 // Set to default image if empty
 if( empty($img) ) $img = Theme::asset('images/banners/default.jpg');
 ?>
 <!-- Begin page banner -->
 <div class="banner" role="banner">
     <!-- Content Layer -->
-    <div class="content"></div>
+    <div class="content"><?= isset($banner['content']) ? $banner['content'] : '' ?></div>
     <!-- Image Overlay Layer -->
     <div class="overlay"></div>
     <!-- Image Layer -->
