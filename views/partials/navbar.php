8990e185a33f1cf966bf4351c4b67e7ec9584707
Nathan Malachowski <nathanmal@gmail.com>
2018-04-11 02:13:12 -0500

    restore navbar to astro version

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,24 +1,39 @@
-<header class="header" role="banner">
+<header class="header">
 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+	<nav class="navbar navbar-expand-lg fixed-top hide" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 
 		<div class="container-fluid">
 
-		<a class="navbar-brand" href="<?=get_site_url()?>/">
-			<?php bloginfo('name'); ?>
-		</a>
-
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	       <span class="navbar-toggler-icon"></span>
-	    </button>
+	  </button>
 		
 		<div class="collapse navbar-collapse">
 			<?php 
-				Theme::menu('main', array('menu_class'=>'navbar-nav mx-auto'));
+			Theme::menu('main', array('menu_class'=>'navbar-nav mx-auto'));
 			?>
 		</div>
 		
 		</div>
 	</nav>
+
+
+	<div class="page-header">
+			<div class="container no-padding">
+					<div class="page-nav">
+							<button class="btn btn-inverse">
+								<?php the_title();?> <i class="fas fa-chevron-circle-down"></i>
+							</button>
+							<?php Theme::menu('main', array('menu_class'=>'nav'));?>
+					</div>
+			</div>
+	</div>
+
+
+	<div id="drop-menu">
+			<?php Theme::menu('main', array('menu_class'=>'nav'));?>
+			<a href="#" class="close"><i class="fas fa-times-circle"></i></a>
+	</div>
+
 
 </header>
