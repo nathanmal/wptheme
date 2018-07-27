1d901f56080750ca6f833939ce40628432741696
Nathan Malachowski <nathanmal@gmail.com>
2018-07-27 14:33:45 -0500

    fixed php tags

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 <!-- Global Site Footer -->
 <footer>
   <div class="container no-padding">
-      <p class="copyright">All content and media copyright &copy; bloginfo('name') <?= date('Y');?></p>
+      <p class="copyright">All content and media copyright &copy; <?= bloginfo('name') . date('Y') ?></p>
 
       <?php echo Theme::getTemplate();?>
   </div>
