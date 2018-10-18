55e888e04ffafb86333dec43fb7a84e2711f332c
Nathan Malachowski <nathanmal@gmail.com>
2018-10-17 19:57:39 -0500

    view updates

--- views/footer.php
+++ views/footer.php
@@ -1,9 +1,12 @@
 <!-- Global Site Footer -->
 <footer>
-  <div class="container no-padding">
-      <p class="copyright">All content and media copyright &copy; <?= bloginfo('name') . ' ' . date('Y') ?></p>
-
-      <?php echo Theme::getTemplate();?>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <p class="copyright">All content and media copyright &copy; <?= bloginfo('name') . ' ' . date('Y') ?></p>
+        <?= THEME_DEBUG ? Theme::getTemplate() : '' ?>
+      </div>
+    </div>
   </div>
 </footer>
 <!-- WP Footer -->
--- views/templates/page.php
+++ views/templates/page.php
@@ -5,14 +5,14 @@ Theme::partial('navbar');
 if( have_posts() ) the_post(); 
 ?>
 
-<div class="container-fluid no-padding">
+<div class="container-fluid">
     
     <?php Theme::partial('banner'); ?>
     
-        <main id="content" class="container" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Webpage">
-            
-            <?php the_content(); ?>
+    <main id="content" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Webpage">
+        
+      <?php the_content(); ?>
 
-        </main><!-- End main#content -->
+    </main><!-- End main#content -->
 
 </div><!-- End #page.page-wrapper -->
