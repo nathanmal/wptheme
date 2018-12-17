3dab940814c11f307f4edde38ab536465f41b4f3
Nathan Malachowski <nathanmal@gmail.com>
2018-12-16 18:02:28 -0600

    update assets

--- assets/src/js/theme.js
+++ assets/src/js/theme.js
@@ -1,7 +1,10 @@
-import 'bootstrap';
+// Import stylesheets (will be combined in order)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import '../scss/theme.scss';
-
+// Import libraries
+import 'bootstrap/dist/js/bootstrap.min.js';
 // Fire when ready
-jQuery(function($){
+jQuery(function($) {
     console.log('Theme script ready');
 });
