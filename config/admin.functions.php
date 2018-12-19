4384d66d4276f4822a0e245ba32e58bbaf4a0f00
Nathan Malachowski <nathanmal@gmail.com>
2018-12-18 21:49:34 -0600

    add theme customization hook

--- config/admin.functions.php
+++ config/admin.functions.php
@@ -1,2 +1,8 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Theme customization
+add_action('customize_register', array('Customize','init') );
+
+
 // Add your custom admin-only functions here
+
