9c4b96a5d24be927e3ae878e373593629f1c87f3
Nathan Malachowski <nathanmal@gmail.com>
2018-12-19 22:30:28 -0600

    update assets

--- assets/src/js/theme.js
+++ assets/src/js/theme.js
@@ -1,8 +1,11 @@
 // Import stylesheets (will be combined in order)
+// Bootstrap 4
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Font awesome
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+// Theme SASS
 import '../scss/theme.scss';
-// Import libraries
+// Bootstrap JS
 import 'bootstrap/dist/js/bootstrap.min.js';
 // Fire when ready
 jQuery(function($) {
