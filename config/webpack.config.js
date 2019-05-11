a516adfa45d8208e0be80f29372089f9ec32d655
Nathan Malachowski <nathanmal@gmail.com>
2019-05-11 00:49:03 -0500

    removed loader file, updated to Terser plugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,9 +3,8 @@ const webpack                 = require('webpack');
 const path                    = require('path');
 const MiniCssExtractPlugin    = require("mini-css-extract-plugin");
 const CleanWebpackPlugin      = require('clean-webpack-plugin');
-const UglifyJsPlugin          = require("uglifyjs-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const loader                  = require('./loader.js'); 
+const TerserPlugin            = require('terser-webpack-plugin');
 
 // Webpack config
 module.exports = (env, argv) => {
@@ -38,13 +37,13 @@ module.exports = (env, argv) => {
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loader: loader('babel')
+          loader: 'babel-loader'
         },
         
         // Images
         {
           test: /images\/.*\.(png|jpg|gif)$/,
-          loader: loader('file'),
+          loader: 'file-loader',
           options: {
             name(file) {
               const filename = production ? '[hash].[ext]' : '[name].[ext]';
@@ -57,7 +56,7 @@ module.exports = (env, argv) => {
         {
            test: /fonts\/.*\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
            use: [{
-             loader: loader('file'),
+             loader: 'file-loader',
              options: {
                name: 'fonts/[name].[ext]',
              }
@@ -67,11 +66,12 @@ module.exports = (env, argv) => {
         // Application Files
         {
           test: /app\/.*\.(png|jpg|ico|xml|webmanifest|xml)$/,
-          loader: loader('file'),
+          loader: 'file-loader',
           options: {
             name(file) {
-              const filename = production ? '[hash].[ext]' : '[name].[ext]';
-              return 'app/' + filename;
+              //const filename = production ? '[hash].[ext]' : '[name].[ext]';
+              // const filename = production ? '[hash].[ext]' : ;
+              return 'app/[name].[ext]';
             },
           }
         },
@@ -80,11 +80,23 @@ module.exports = (env, argv) => {
         {
           test: /\.[s]?css$/,
           use: [
-            loader('style'),
-            loader('extract'),
-            loader('css'),
-            loader('sass'),
-            loader('postcss'),
+            'style-loader',
+            MiniCssExtractPlugin.loader,
+            { 
+              loader: 'css-loader',
+                options: { 
+                  importLoaders: 2
+                }
+            },
+            'sass-loader',
+            { 
+              loader : 'postcss-loader',
+              options: {
+                config : { 
+                  path : path.resolve( __dirname, 'postcss.config.js' )
+                }
+              }
+            },
           ]
         },
         
@@ -93,25 +105,44 @@ module.exports = (env, argv) => {
 
     // Plugins
     plugins: [
+      new webpack.ProgressPlugin(),
       // clean distro
-      new CleanWebpackPlugin('dist', {}),
+      new CleanWebpackPlugin(),
       // css file name
       new MiniCssExtractPlugin({ filename: '[name].css' }),
       // external jquery
-      new webpack.ProvidePlugin({  $: 'jquery', jQuery: 'jquery' })
-    ]
-    
+      new webpack.ProvidePlugin({  $: 'jquery', jQuery: 'jquery' }), 
+
+      //new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: true }),
+    ],
+
+    performance: {
+      hints: false
+    }
+
+
   }
 
   // Add optimization in production
-  if( production ){
+  
+  if( production )
+  {
     config.optimization = {
       minimizer: [
-        new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: true }),
+        new TerserPlugin({
+          test: /\.js(\?.*)?$/i,
+          cache: true,
+          parallel: true,
+          sourceMap: true
+          // extractComments: true,
+        }),
         new OptimizeCSSAssetsPlugin({})
       ]
     }
+
+
   }
+  
 
   // return webpack config object
   return config;
